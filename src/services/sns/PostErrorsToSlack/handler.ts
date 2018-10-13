7734946a3592b580c175700544ba60c290beb30a
Hassan Khan <hassan.khan@endemolshine.com>
2018-10-13 15:20:28 +0100

    refactor(handler): extract mapping function

--- src/services/sns/PostErrorsToSlack/handler.ts
+++ src/services/sns/PostErrorsToSlack/handler.ts
@@ -1,23 +1,20 @@
-import { SNSEvent } from 'aws-lambda';
+import { SNSEvent, SNSEventRecord } from 'aws-lambda';
 
 import createSlackMessage from '@lib/createSlackMessage';
 import mapStatus from '@lib/mapStatus';
 import publishToSlack from '@lib/publishToSlack';
 import { SlackStatus, SNSMessage } from '@src/types';
 
-export default async (event: SNSEvent) => {
-  const slackMessages = event.Records.map((record) => {
-    const message: SNSMessage = JSON.parse(record.Sns.Message);
-    const status: SlackStatus = mapStatus(message.NewStateValue);
-    return createSlackMessage(message, status);
-  });
+const processRecord = async (record: SNSEventRecord) => {
+  const message: SNSMessage = JSON.parse(record.Sns.Message);
+  const status: SlackStatus = mapStatus(message.NewStateValue);
+  const slackMessage = await createSlackMessage(message, status);
+  return publishToSlack(slackMessage);
+};
 
+export default async (event: SNSEvent) => {
   try {
-    await Promise.all(
-      slackMessages.map((slackMessage) => {
-        return publishToSlack(slackMessage);
-      }),
-    );
+    await Promise.all(event.Records.map(processRecord));
   } catch (error) {
     return error;
   }
