1b49ee3de046cbd9611c86ad6cea81022e13d81e
Hassan Khan <hassan.khan@endemolshine.com>
2018-11-09 01:05:32 +0000

    ci(codebuild): add webhook configuration

--- scripts/deploy-codebuild.sh
+++ scripts/deploy-codebuild.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 aws cloudformation deploy --template-file conf/template.yml --stack-name serverless-stack-notifier-codebuild --region eu-west-1 --capabilities CAPABILITY_NAMED_IAM
+aws codebuild update-webhook --project-name serverless-stack-notifier --branch-filter ^develop|master$ --region eu-west-1
