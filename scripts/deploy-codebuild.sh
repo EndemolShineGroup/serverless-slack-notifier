da250280974c15e5373cff9869c2cf19521655ff
Hassan Khan <hassan.khan@endemolshine.com>
2018-11-11 00:56:25 +0000

    fix(scripts): add sleep section after deploying template

--- scripts/deploy-codebuild.sh
+++ scripts/deploy-codebuild.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-aws cloudformation deploy --template-file conf/template.yml --stack-name serverless-stack-notifier-codebuild --region eu-west-1 --capabilities CAPABILITY_NAMED_IAM
-aws codebuild update-webhook --project-name serverless-stack-notifier --branch-filter ^develop|master$ --region eu-west-1
+aws cloudformation deploy --template-file conf/template.yml --stack-name "serverless-stack-notifier-codebuild" --region us-east-1 --capabilities CAPABILITY_NAMED_IAM
+sleep 5
+aws codebuild update-webhook --project-name "serverless-slack-notifier" --region us-east-1 --branch-filter "^develop|master$"
